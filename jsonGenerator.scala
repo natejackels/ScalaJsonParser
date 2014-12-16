6fc2a0b5c0f567a91d48cb968e02a39e92064921
fiffer <njfisher@mtu.edu>
2014-12-16 03:47:02 -0500

    jsonGenerator
    Updated performance a bit. And wrote up discovery of Generator issues I
    ran into

--- jsonGenerator.scala
+++ jsonGenerator.scala
@@ -1,3 +1,43 @@
-object jsonGenerator {
+package generator
+import scala.actors.Actor._
+import java.util.ArrayList
+import java.util.HashMap
+import java.util.Scanner
 
+object jsonGenerator {
+	def loads(in:Any):String = {
+			val finalVal = "Temp holder"
+			if(in.getClass.getSimpleName == "HashMap"){
+			   
+			  
+			}
+			if(in.getClass.getSimpleName == "Array"){
+			  
+			}
+			
+			//We might need to switch up our implementation. 
+			//I have tried to get this working using the Any type, 
+			//and so far it seems impossible for scala to handle
+			//multiple types (in terms of pulling data from it)
+			
+			//I also tried switching up jasonParser to return 
+			//more specific data types. However it seems the 
+			//max you can return is 2 using Either. 
+			
+			//So what we might have to do, is return the results
+			//directly to a specific Generator for each possible
+			//return type. The only problem with this is if
+			//any "Any" types are nested within the HashMap
+			// or Array, etc. They wont be able to be converted
+			//to their native type (at least from what i have tried
+			
+			//Going to head to bed for now. But will work on this
+			//tomorrow
+			
+			//If you want to dabble, in.getClass. has some interesting
+			//features that might be something that could help. 
+			//I couldn't find anything specific though that would allow
+			//the casting to the native type. 
+			return finalVal;
+	}
 }
--- performance.scala
+++ performance.scala
@@ -1,22 +1,24 @@
-import parse.jsonParser
-
+import parser.jsonParser
+import generator.jsonGenerator
 object performance {
 	def main(args: Array[String]): Unit = {
 		var a = 0
-		var startTime : Long = 0
-		var endTime : Long = 0
-		var runTime : Long = 0
-		var totalTime: Long = 0
-
+		var startTime : Double = 0
+		var endTime : Double = 0
+		var runTime : Double = 0
+		var totalTime: Double = 0
+		var result2 = ""
 		for (a <- 1 to 10002){
 			startTime = java.lang.System.currentTimeMillis()
 			var result = jsonParser.loads("""{"id": 1,"name": "A green door","price": 12.50,"tags": ["home", "green"] }""")
+			//result2 = jsonGenerator.loads(result)
 			endTime = java.lang.System.currentTimeMillis()
 			if (a > 2){
 				runTime = endTime-startTime
 				totalTime += runTime
 			}
 		}
-		println(totalTime/10000)
+		//println (result2)
+		println(totalTime/1000 + " Seconds for tests")
   	}
 }
